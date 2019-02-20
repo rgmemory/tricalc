ce132054e747650af97a57e12249ce5aa3d9ff58
Russell Memory <rmemory@Russells-MacBook-Pro-2.local>
2019-02-20 12:43:50 -0700

    hosting

--- server/index.js
+++ server/index.js
@@ -1,7 +1,15 @@
 const express = require('express')
+const path = require('path'); // Usually moved to the start of file
 
 const app = express();
+
+
 
 app.listen(3400, () => {
     console.log('listening on port 3400')
 })
+
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname, '../build/index.html'));
+});
